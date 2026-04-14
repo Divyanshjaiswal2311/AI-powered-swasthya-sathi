c4be4d11cedd1b8290252cd8ee68d5b989d04f27
Divyanshu Jaiswal <CSEFW2206@glbitm.ac.in>
2026-04-15 00:54:31 +0530

    Update medicalRecordRoutes.js

--- routes/medicalRecordRoutes.js
+++ routes/medicalRecordRoutes.js
@@ -7,7 +7,7 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const authMiddleware = require("../middlewares/authMiddelware");
+const authMiddleware = require("../middlewares/authMiddleware");
 const {
   uploadMedicalRecordController,
   getMedicalRecordsController,
